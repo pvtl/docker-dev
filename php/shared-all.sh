9f3126e1ed81bb9b8735f67aa277acf8bdceb0ed
Adi Naidu <adi.n@pivotalagency.com.au>
2019-01-23 09:34:08 +1000

    Revert "Changes by Kyle on Adi's machine"
    This reverts commit 1ecc4add7615eda5c1d35dfcedfe516a951b7823.

--- php/shared-all.sh
+++ php/shared-all.sh
@@ -1,32 +1,17 @@
 # Install Composer and Prestissimo
-EXPECTED_SIGNATURE="$(curl -s https://composer.github.io/installer.sig)"
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
-
-if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
-then
-  >&2 echo 'ERROR: Invalid installer signature'
-  rm composer-setup.php
-  exit 1
-fi
-
-php composer-setup.php --quiet
-RESULT=$?
-rm composer-setup.php
-mv composer.phar /usr/local/bin/composer
-exit $RESULT
-
-composer global require hirak/prestissimo
+curl --silent --show-error https://getcomposer.org/installer | php \
+    && mv composer.phar /usr/local/bin/composer \
+    && composer global require hirak/prestissimo
 
 # Install Node
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash - \
-  && apt-get install -y nodejs \
-  && apt-get clean \
-  && rm -rf /var/lib/apt/lists/*
+curl -sL https://deb.nodesource.com/setup_10.x | bash - \
+    && apt-get install -y nodejs \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
 
 # Install useful NPM CLIs
 npm i -g grunt-cli \
-  && npm i -g gulp-cli
+    && npm i -g gulp-cli
 
 # Install Yarn
 curl -sS 'https://dl.yarnpkg.com/debian/pubkey.gpg' | apt-key add - \
@@ -44,15 +29,15 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
   && mv wp-cli.phar /usr/local/bin/wp
 
 # Blackfire PHP Profiler
-version=$(php -r "echo PHP_MAJOR_VERSION.PHP_MINOR_VERSION;") \
-  && curl -A "Docker" -o /tmp/blackfire-probe.tar.gz -D - -L -s https://blackfire.io/api/v1/releases/probe# /php/linux/amd64/$version \
-  && tar zxpf /tmp/blackfire-probe.tar.gz -C /tmp \
-  && mv /tmp/blackfire-*.so $(php -r "echo ini_get('extension_dir');")/blackfire.so \
-  && printf "extension=blackfire.so\nblackfire.agent_socket=tcp://blackfire:8707\n" > $PHP_INI_DIR/conf.d/blackfire.ini
+# version=$(php -r "echo PHP_MAJOR_VERSION.PHP_MINOR_VERSION;") \
+#     && curl -A "Docker" -o /tmp/blackfire-probe.tar.gz -D - -L -s https://blackfire.io/api/v1/releases/probe# /php/linux/amd64/$version \
+#     && tar zxpf /tmp/blackfire-probe.tar.gz -C /tmp \
+#     && mv /tmp/blackfire-*.so $(php -r "echo ini_get('extension_dir');")/blackfire.so \
+#     && printf "extension=blackfire.so\nblackfire.agent_socket=tcp://blackfire:8707\n" > $PHP_INI_DIR/conf.d/blackfire.ini
 
 # Setup Crons
 touch /root/custom_crontab \
-  && /usr/bin/crontab -u www-data /root/custom_crontab
+    && /usr/bin/crontab -u www-data /root/custom_crontab
 
 # Update site's directory permissions
 chown -R www-data /var/www/
