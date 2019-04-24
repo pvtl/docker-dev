e07904f4c479ae84d1ca7b008769feadc93b376e
Jeff <jeff@jeffmiller.com.au>
2019-04-24 11:10:50 +1000

    Fix Composer and Node/NPM install scripts

--- php/shared-all.sh
+++ php/shared-all.sh
@@ -1,25 +1,10 @@
 # Install Composer and Prestissimo
-EXPECTED_SIGNATURE="$(curl -s https://composer.github.io/installer.sig)"
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
-
-if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
-then
-  >&2 echo 'ERROR: Invalid installer signature'
-  rm composer-setup.php
-  exit 1
-fi
-
-php composer-setup.php --quiet
-RESULT=$?
-rm composer-setup.php
-mv composer.phar /usr/local/bin/composer
-exit $RESULT
-
-composer global require hirak/prestissimo
+curl --silent --show-error https://getcomposer.org/installer | php \
+  && mv composer.phar /usr/local/bin/composer \
+  && composer global require hirak/prestissimo
 
 # Install Node
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash - \
+curl -sL https://deb.nodesource.com/setup_10.x | bash - \
   && apt-get install -y nodejs \
   && apt-get clean \
   && rm -rf /var/lib/apt/lists/*
