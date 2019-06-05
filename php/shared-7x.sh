baf26e5e8ba7a5eab75f09852fb85878f301d558
Matt McNamee <matt@mcnamee.co>
2019-06-05 11:00:26 +1000

    Add exif PHP extension

--- php/shared-7x.sh
+++ php/shared-7x.sh
@@ -23,8 +23,8 @@ apt-get update && apt-get install -y \
         --no-install-recommends \
     && yes '' | pecl install -f redis xdebug mcrypt \
     && docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/ \
-    && docker-php-ext-install -j$(nproc) bcmath calendar gd intl mysqli opcache pdo_mysql soap xsl zip \
-    && docker-php-ext-enable redis xdebug mcrypt bcmath calendar gd intl mysqli opcache pdo_mysql soap xsl zip \
+    && docker-php-ext-install -j$(nproc) bcmath calendar exif gd intl mysqli opcache pdo_mysql soap xsl zip \
+    && docker-php-ext-enable redis exif xdebug mcrypt bcmath calendar gd intl mysqli opcache pdo_mysql soap xsl zip \
     && mv /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini.DISABLE \
     && apt-get clean \
     && rm -rf /var/lib/apt/lists/*
