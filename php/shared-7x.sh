1cb4de6f4d711afb88dc4a16169d7ac79a734859
Matt McNamee <matt@mcnamee.co>
2020-01-06 12:37:51 +1000

    Add PHP 7.3

--- php/shared-7x.sh
+++ php/shared-7x.sh
@@ -16,23 +16,16 @@ apt-get update && apt-get install -y \
         nano \
         net-tools \
         ssh \
-        ssmtp \
+        msmtp \
         supervisor \
         unzip \
-        zip \
+        libzip-dev \
+        libz-dev \
         --no-install-recommends \
-    && yes '' | pecl install -f redis xdebug mcrypt \
+    && yes '' | pecl install -f redis memcached mcrypt xdebug \
     && docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/ \
     && docker-php-ext-install -j$(nproc) bcmath calendar exif gd intl mysqli opcache pdo_mysql soap xsl zip \
-    && docker-php-ext-enable redis exif xdebug mcrypt bcmath calendar gd intl mysqli opcache pdo_mysql soap xsl zip \
+    && docker-php-ext-enable redis exif xdebug mcrypt bcmath calendar gd intl memcached mysqli opcache pdo_mysql soap xsl zip \
     && mv /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini.DISABLE \
     && apt-get clean \
     && rm -rf /var/lib/apt/lists/*
-
-# Memcache
-curl -L -o /tmp/memcached.tar.gz "https://github.com/php-memcached-dev/php-memcached/archive/php7.tar.gz" \
-    && mkdir -p /usr/src/php/ext/memcached \
-    && tar -C /usr/src/php/ext/memcached -zxvf /tmp/memcached.tar.gz --strip 1 \
-    && docker-php-ext-configure memcached \
-    && docker-php-ext-install memcached \
-    && rm /tmp/memcached.tar.gz
