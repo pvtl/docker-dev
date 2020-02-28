9aebeed30d1c7ed198c49b651211c70cc148ea25
Matt McNamee <matt@mcnamee.co>
2020-02-28 14:30:53 +1000

    Fix PHP 7.0 xdebug

--- php/shared-7x.sh
+++ php/shared-7x.sh
@@ -1,3 +1,7 @@
+# Dermine Xdebug version (PHP 7.0- requires < xdebug-2.6.1)
+PHP_VERSION=$(php -r "echo substr(str_replace('.', '', phpversion()), 0, 3);")
+[ $PHP_VERSION -gt 710 ] && XDEBUG_MODULE="xdebug" || XDEBUG_MODULE="xdebug-2.6.1"
+
 # Required PHP Extensions
 apt update && apt install -y --no-install-recommends \
         cron \
@@ -21,7 +25,7 @@ apt update && apt install -y --no-install-recommends \
         unzip \
         libzip-dev \
         libz-dev \
-    && yes '' | pecl install -f redis memcached mcrypt xdebug \
+    && yes '' | pecl install -f redis memcached mcrypt $XDEBUG_MODULE \
     && docker-php-ext-install -j$(nproc) bcmath calendar exif gd intl mysqli opcache pdo_mysql soap xsl zip \
     && docker-php-ext-enable redis exif xdebug mcrypt bcmath calendar gd intl memcached mysqli opcache pdo_mysql soap xsl zip \
     && mv /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini.DISABLE \
