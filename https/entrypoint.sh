5df6be3dd7e9702a22738547280682696aae992b
Matt McNamee <matt@mcnamee.co>
2020-03-24 15:21:43 +1000

    Fixes to HTTPs

--- https/entrypoint.sh
+++ https/entrypoint.sh
@@ -2,6 +2,6 @@
 
 set -eu
 
-envsubst '$$HOST_IP' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '$$PORT$$HOST_IP' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 cat /etc/nginx/nginx.conf
 nginx
