f7c0b599b0832da046867c3ee8961631ac95aa29
Kyle Stevenson <kyle.stevenson@gmail.com>
2023-11-27 11:55:15 +1000

    Add PHP 8.3 support

--- php/src/build-n-push.sh
+++ php/src/build-n-push.sh
@@ -23,7 +23,16 @@ docker buildx create --name pvtl --use
 docker buildx create --name pvtl --platform linux/amd64 --append ssh://pvtl@192.168.0.5 # Leverage the dev server
 
 
-# Let's start building!
+# PHP 8.3 (2023)
+docker buildx build --no-cache --pull -f 83/Dockerfile --platform linux/arm64,linux/amd64 -t wearepvtl/php-fpm-8.3:latest --push .
+
+if [[ ${TAG} != "latest" ]] ; then
+  docker tag wearepvtl/php-fpm-8.3 wearepvtl/php-fpm-8.3:${TAG}
+  docker push wearepvtl/php-fpm-8.3:${TAG}
+fi
+
+
+# PHP 8.2 (2022)
 docker buildx build --no-cache --pull -f 82/Dockerfile --platform linux/arm64,linux/amd64 -t wearepvtl/php-fpm-8.2:latest --push .
 
 if [[ ${TAG} != "latest" ]] ; then
@@ -31,6 +40,8 @@ if [[ ${TAG} != "latest" ]] ; then
   docker push wearepvtl/php-fpm-8.2:${TAG}
 fi
 
+
+# PHP 8.1 (2021)
 docker buildx build --no-cache --pull -f 81/Dockerfile --platform linux/arm64,linux/amd64 -t wearepvtl/php-fpm-8.1:latest --push .
 
 if [[ ${TAG} != "latest" ]] ; then
@@ -38,13 +49,17 @@ if [[ ${TAG} != "latest" ]] ; then
   docker push wearepvtl/php-fpm-8.1:${TAG}
 fi
 
-docker buildx build --no-cache --pull -f 80/Dockerfile --platform linux/arm64,linux/amd64 -t wearepvtl/php-fpm-8.0:latest --push .
 
-if [[ ${TAG} != "latest" ]] ; then
-  docker tag wearepvtl/php-fpm-8.0 wearepvtl/php-fpm-8.0:${TAG}
-  docker push wearepvtl/php-fpm-8.0:${TAG}
-fi
+# PHP 8.0 (2020)
+# docker buildx build --no-cache --pull -f 80/Dockerfile --platform linux/arm64,linux/amd64 -t wearepvtl/php-fpm-8.0:latest --push .
 
+# if [[ ${TAG} != "latest" ]] ; then
+#   docker tag wearepvtl/php-fpm-8.0 wearepvtl/php-fpm-8.0:${TAG}
+#   docker push wearepvtl/php-fpm-8.0:${TAG}
+# fi
+
+
+# PHP 7.4 (2019)
 # docker buildx build --no-cache --pull -f 74/Dockerfile --platform linux/arm64,linux/amd64 -t wearepvtl/php-fpm-7.4:latest --push .
 
 # if [[ ${TAG} != "latest" ]] ; then
@@ -52,6 +67,8 @@ fi
 #   docker push wearepvtl/php-fpm-7.4:${TAG}
 # fi
 
+
+# PHP 7.3 (2018)
 # docker buildx build --no-cache --pull -f 73/Dockerfile --platform linux/arm64,linux/amd64 -t wearepvtl/php-fpm-7.3:latest --push .
 
 # if [[ ${TAG} != "latest" ]] ; then
@@ -59,6 +76,8 @@ fi
 #   docker push wearepvtl/php-fpm-7.3:${TAG}
 # fi
 
+
+# PHP 7.2 (2017)
 # docker buildx build --no-cache --pull -f 72/Dockerfile --platform linux/arm64,linux/amd64 -t wearepvtl/php-fpm-7.2:latest --push .
 
 # if [[ ${TAG} != "latest" ]] ; then
@@ -66,6 +85,8 @@ fi
 #   docker push wearepvtl/php-fpm-7.2:${TAG}
 # fi
 
+
+# PHP 7.1 (2016)
 # docker buildx build --no-cache --pull -f 71/Dockerfile --platform linux/arm64,linux/amd64 -t wearepvtl/php-fpm-7.1:latest --push .
 
 # if [[ ${TAG} != "latest" ]] ; then
@@ -73,6 +94,8 @@ fi
 #   docker push wearepvtl/php-fpm-7.1:${TAG}
 # fi
 
+
+# PHP 7.0 (2015)
 # docker buildx build --no-cache --pull -f 70/Dockerfile --platform linux/arm64,linux/amd64 -t wearepvtl/php-fpm-7.0:latest --push .
 
 # if [[ ${TAG} != "latest" ]] ; then
@@ -80,12 +103,15 @@ fi
 #   docker push wearepvtl/php-fpm-7.0:${TAG}
 # fi
 
+
+# PHP 5.6 (2014)
 # docker buildx build --no-cache --pull -f 56/Dockerfile --platform linux/arm64,linux/amd64 -t wearepvtl/php-fpm-5.6:latest --push .
 
 # if [[ ${TAG} != "latest" ]] ; then
 #   docker tag wearepvtl/php-fpm-5.6 wearepvtl/php-fpm-5.6:${TAG}
 #   docker push wearepvtl/php-fpm-5.6:${TAG}
 # fi
+
 
 # Tear down build servers
 docker buildx rm pvtl
